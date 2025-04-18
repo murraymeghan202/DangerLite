09c0cc5a6ea02c13aa84c77c0d06f4cd23fdab0e
murraymeghan202 <murrayalex2024.5@gmail.com>
2025-04-18 12:25:04 -0400

    Update turtle.lua

--- turtle.lua
+++ turtle.lua
@@ -13,15 +13,8 @@ while true
   for(current_loc, displacement, 1) do
     local forward = turtle.forward()
   if (forward == false and turtle.getFuelLimit() == 0) then
-     rednet.open("left")
-     local turtle_fuel_id = rednet.lookup("TurtleFuel", "TurtleControl")
-     if (turtle_fuel_id) then
-        local init_msg = rednet.receive("TurtleFuel")
-        if(init_msg ~= nil) then
-          rednet.broadcast("Turtle needs fuel!")
-      end
-    end   
-    else
+    turtle.refuel() 
+  else
       if (turtle.detect()) then
         turtle.dig()
       end
