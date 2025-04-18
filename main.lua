042179b1932269a75704b0c72d9c28b3eb14fa12
murraymeghan202 <murrayalex2024.5@gmail.com>
2025-04-18 12:03:32 -0400

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,22 @@
+-- Minecraft coordinates are X,Y,Z
+
+-- leave this alone
+local current_loc = vector.new(gps.locate(1500, false))
+
+-- edit this
+local home_loc = vector.new(100, 100, 100)
+
+-- leave this alone
+local displacement = current_loc - home_loc
+
+while true
+  for(current_loc, displacement, 1) do
+    local forward = turtle.forward()
+  if (forward == false and turtle.getFuelLimit() == 0) then
+      print("Turtle needs more fuel!)
+    else
+      turtle.dig()
+    end
+  end
+end
+
